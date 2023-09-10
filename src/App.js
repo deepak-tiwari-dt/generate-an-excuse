34a51f63f05275161468cf264dad5e1a1f9d8f74
Deepak Tiwari <dt@Deepaks-MacBook-Air.local>
2023-09-10 17:04:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import {useState} from 'react';
 
 function App() {
+  const [generatedExcuse, setGeneratedExcuse] = useState('');
+
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}`).then((res) => {
+      setGeneratedExcuse(res.data[0].excuse);
+    });
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Generate An Excuse</h1>
+      <button onClick={() => fetchExcuse("family")}>Family</button>
+      <button onClick={() => fetchExcuse("office")}>Office</button>
+      <button onClick={() => fetchExcuse("children")}>Children</button>
+      <button onClick={() => fetchExcuse("college")}>College</button>
+      <button onClick={() => fetchExcuse("party")}>Party</button>
+      <button onClick={() => fetchExcuse("funny")}>Funny</button>
+      <button onClick={() => fetchExcuse("unbelievable")}>Unbelievable</button>
+      <button onClick={() => fetchExcuse("developers")}>Developers</button>
+      <button onClick={() => fetchExcuse("gaming")}>Gaming</button>
+      <p>{generatedExcuse}</p>
     </div>
   );
 }
